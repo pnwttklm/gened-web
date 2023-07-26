36ddd44e6c8440d2fd76d22c42aa1c0f69297b68
S4NHXNU1 <93254994+S4NHXNU1@users.noreply.github.com>
2023-07-26 17:25:52 +0700

    Update ServerSide API

--- backend-mssql/server.js
+++ backend-mssql/server.js
@@ -12,5 +12,39 @@ const config = {
     user: 'sa',
     password: 's4nhxnu1',
     server: 'server.s4nhxnu1.com',
-    database: 'your_database',
+    database: 'MUGenED',
+    options: {
+        trustServerCertificate: true
+    }
   };
+
+app.get('/api/data/course', async (req, res) => {
+try {
+    // Receive parameter
+    const ID = req.query.id;
+    const SKIP = Number(req.query.pageNum) * 10;
+
+    // Connect to the MSSQL database
+    const pool = await sql.connect(config);
+
+    // Setup Query String
+    var query = `SELECT * FROM TrCourse`;
+    if(ID != "") query += ` WHERE ID LIKE '%${ID}%'`;
+    query += ` ORDER BY ID OFFSET ${SKIP} ROWS FETCH NEXT 10 ROWS ONLY;`;
+
+    // Query the data from the database
+    const result = await pool.request().query(query);
+
+    // Close the database connection
+    sql.close();
+
+    res.json(result.recordset);
+} catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
+}
+});
+
+app.listen(port, () => {
+console.log(`Server is running on http://localhost:${port}`);
+});
